c43b7dd278b3e36306a450e606af66a10888b9f8
Le Trung Hieu <hieulebk0609@gmail.com>
2024-01-23 21:03:10 +0700

    fix bug 127.0.0.1

--- src/config/config.js
+++ src/config/config.js
@@ -25,14 +25,14 @@ module.exports = {
     username: "root",
     password: null,
     database: "database_test",
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
 };
